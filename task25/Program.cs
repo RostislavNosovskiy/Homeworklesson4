e93969d43160bc40f2605194e9b7301f28d2e058
Rosislav <rostislavnosovskij467@gmail.com>
2023-04-04 00:30:49 +0300

    Домашнее задание к четвёртому семинару

---
+++ task25/Program.cs
@@ -0,0 +1,17 @@
+﻿/***Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+Console.Clear();
+int Exponentiation (int A ,int B){
+int exp = 1;
+for (int i=0; i<B; i++)
+{
+    exp = exp*A;
+}
+return exp;
+}
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"Число {A} в степени {B} равно {Exponentiation (A ,B)}");
---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿/*** Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+Console.Clear();
+int sum(int n){
+    int s = 0;
+    while (n>0)
+    {
+      s = s + n%10;
+      n = n/10;
+    }
+    return s;
+}
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int a = n;
+Console.WriteLine($"{a} -> {sum(n)}");
